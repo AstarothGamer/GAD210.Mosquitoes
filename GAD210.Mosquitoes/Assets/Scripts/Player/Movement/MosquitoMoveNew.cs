cb3b89a2087faa8aae44d6c03dfaa5b3bf5293b6
AlexandrTimchenko <aleksandar.timchenko@student.saedubai.com>
2025-04-21 21:56:02 +0900

    Scripts restoration

---
+++ GAD210.Mosquitoes/Assets/Scripts/Player/Movement/MosquitoMoveNew.cs
@@ -0,0 +1,196 @@
+using System.Collections;
+using UnityEngine;
+
+public class MosquitoMoveNew : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float speed = 4;
+    [SerializeField] private float boostSpeed = 8;
+    [SerializeField] private float mouseSensitivity = 3f;
+    [SerializeField] private float xRotation = 0;
+    [SerializeField] private float yRotation;
+    [SerializeField] private int dodgingDistance = 2;
+    [SerializeField] private float energy = 5f;
+    [SerializeField] private float timeDuration = 3;
+    [Header("Other")]
+    [SerializeField] private bool isBoostActive = false;
+    [SerializeField] private Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private Abilities abilities;
+    [SerializeField] private MosquitoAlign aligner;
+    [SerializeField] private Transform cam;
+
+
+
+    void Start()
+    {
+
+        if (cam == null)
+        {
+
+            cam = Camera.main.transform;
+
+        }
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energy += Time.deltaTime;
+
+        RotateToCamera();
+
+        if (abilities != null && abilities.isBiting)
+        {
+            return;
+        }
+
+
+
+        if (aligner != null && aligner.isSitting)
+        {
+            return;
+        }
+
+
+
+        Movement();
+
+        Dodging();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isBoostActive && energy >= 8)
+        {
+            StartCoroutine(Boost());
+        }
+
+
+    }
+
+    void Movement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+
+
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        Vector3 forward = cam.forward;
+
+        Vector3 right = cam.right;
+
+        //forward.y = 0;
+
+        //right.y = 0;
+
+       
+
+        forward.Normalize();
+
+        right.Normalize();
+
+
+        moveDirection = forward * vertical + right * horizontal;
+
+
+        moveDirection.Normalize();
+
+
+
+        transform.position += moveDirection * speed * Time.deltaTime;
+
+
+
+        //Vector3 forward = cam.forward;
+
+        //Vector3 right = cam.right;
+
+        //forward.Normalize();
+
+        //right.Normalize();
+
+        //moveDirection = forward * vertical + right * horizontal;
+
+        //moveDirection.Normalize();
+
+        //transform.position += moveDirection * speed * Time.deltaTime;
+
+        // transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+    }
+
+
+    void RotateToCamera()
+    {
+
+
+        Quaternion targetRotation = Quaternion.LookRotation(cam.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+
+
+
+
+
+    }
+
+
+
+
+
+    void Dodging()
+    {
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+
+            float horizontal = Input.GetAxis("Horizontal");
+
+
+            Vector3 dodgeDirection = cam.right * horizontal;
+
+
+            dodgeDirection.y = 0;
+
+            dodgeDirection.Normalize();
+
+            transform.position += dodgeDirection * dodgingDistance;
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+    IEnumerator Boost()
+    {
+        energy = 0;
+
+        isBoostActive = true;
+
+        float originalSpeed = speed;
+
+        speed = boostSpeed;
+
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
+        yield return new WaitForSeconds(timeDuration);
+
+        speed = originalSpeed;
+
+        isBoostActive = false;
+
+
+    }
+}
